de44ac8b6447fb176915424d28016a4116dc8a38
DashaDafusun <84176953+DashaDafusun@users.noreply.github.com>
2021-10-16 15:04:44 +0200

    Add files via upload

---
+++ opdracht_11_eersteJs/script.js
@@ -0,0 +1,28 @@
+
+console.log('Hello Winc_Academy!');
+
+let naam = 'Dasha';
+console.log(naam);
+
+let a = 4;
+let b = 4;
+let c = '4';
+let d = '4';
+console.log(a + b);
+console.log(c + d);
+
+let x = 6;
+let y = 7;
+console.log(x - y);
+console.log(x + y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+let age = 55;
+console.log(typeof age);
+
+let ageInStr = '50';
+console.log(typeof ageInStr);
+
+
